e476432234fa4e1fa538019fe822a1aebf56b10b
Sharon Nzaya <45424644+Nzaya@users.noreply.github.com>
2024-11-10 16:44:15 +0300

    Modified to use all()

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -3,17 +3,17 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Query all books by a specific author
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    books = Book.objects.filter(author=author)  # Using filter to meet the requirement
+    books = Book.objects.filter(author=author).all()  
     return books
 
 # List all books in a library
 def list_books_in_library(library_name):
     library = Library.objects.get(name=library_name)
-    books = Book.objects.filter(library=library)  # Using filter to access books through the ManyToMany relationship
+    books = Book.objects.filter(library=library).all()  
     return books
 
 # Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    librarian = Librarian.objects.filter(library=library).first()  # Access librarian using filter
+    librarian = Librarian.objects.filter(library=library).first() 
     return librarian
