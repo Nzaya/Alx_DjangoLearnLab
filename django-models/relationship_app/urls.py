ae5919b71bd7435ceefe9179842f72693a466afd
Sharon Nzaya <45424644+Nzaya@users.noreply.github.com>
2024-11-10 22:14:51 +0300

    Updated urls

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -19,10 +19,11 @@ urlpatterns = [
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
 
-    path('books/', views.list_books, name='book_list'),
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/<int:book_id>/edit/', views.edit_book, name='edit_book'),
-    path('books/<int:book_id>/delete/', views.delete_book, name='delete_book'),
+    path('books/', views.list_books, name='book_list'),  # List all books
+    path('books/add/', views.add_book, name='add_book'),  # Add a new book
+    path('books/<int:book_id>/edit/', views.edit_book, name='edit_book'),  # Edit an existing book
+    path('books/<int:book_id>/delete/', views.delete_book, name='delete_book'),  # Delete a book
+
 
    
 ]
