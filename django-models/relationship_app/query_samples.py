f4d0c694cfd51e4b1693c32ef431fbc267c0cbd1
Sharon Nzaya <45424644+Nzaya@users.noreply.github.com>
2024-11-10 16:52:08 +0300

    Modified quer samples filter

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -3,17 +3,17 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Query all books by a specific author
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    books = Book.objects.filter(author=author).all()  
+    books = author.books.all() 
     return books
 
 # List all books in a library
 def list_books_in_library(library_name):
     library = Library.objects.get(name=library_name)
-    books = Book.objects.filter(library=library).all()  
+    books = Book.objects.filter(library=library) 
     return books
 
 # Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    librarian = Librarian.objects.filter(library=library).first() 
+    librarian = Librarian.objects.filter(library=library).first()  
     return librarian
