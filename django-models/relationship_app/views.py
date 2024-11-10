f58cb04e165bdd65427887d8378480d7b8e97bbe
Sharon Nzaya <45424644+Nzaya@users.noreply.github.com>
2024-11-10 21:35:19 +0300

    Checks for admin

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -55,15 +55,18 @@ class CustomLogoutView(LogoutView):
 
 # Function to check if the user is an Admin
 def is_admin(user):
-    return user.userprofile.role == 'Admin'
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
 # Function to check if the user is a Librarian
 def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
 
 # Function to check if the user is a Member
 def is_member(user):
-    return user.userprofile.role == 'Member'
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+   
 
 # Admin view (Only accessible by Admin)
 @user_passes_test(is_admin)
