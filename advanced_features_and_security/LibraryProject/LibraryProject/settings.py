b4d294b08f149cf614c0be863404a290d222d0a3
Sharon Nzaya <45424644+Nzaya@users.noreply.github.com>
2024-11-17 22:45:40 +0300

    Adding SECURE_PROXY_SSL_HEADER

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -54,6 +54,9 @@ X_FRAME_OPTIONS = 'DENY'  # Prevent clickjacking
 SECURE_CONTENT_TYPE_NOSNIFF = True  # Prevent MIME-type sniffing
 SECURE_BROWSER_XSS_FILTER = True  # Enable XSS filtering in the browser
 
+# Configure Django to trust the X-Forwarded-Proto header from a proxy
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 ALLOWED_HOSTS = []
 
