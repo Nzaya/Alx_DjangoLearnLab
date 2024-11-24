2a4cb419e5917efb59608d6116f49badf5f229bc
Sharon Nzaya <45424644+Nzaya@users.noreply.github.com>
2024-11-24 11:18:03 +0300

    API Endpoint with Django REST Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookList(ListAPIView):
+    queryset = Book.objects.all()  # Retrieving all Book objects
+    serializer_class = BookSerializer  # Using the BookSerializer for formatting
+
+
