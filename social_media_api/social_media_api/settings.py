01e82ef72b683a36379190ce594d960b9f432cca
Sharon Nzaya <45424644+Nzaya@users.noreply.github.com>
2024-12-15 12:29:34 +0300

    Added posts urls in main project

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',  # for token authentication
-    'accounts',  # custom app
+    'accounts',
+    'posts'
 ]
 
 REST_FRAMEWORK = {
@@ -52,6 +53,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 
